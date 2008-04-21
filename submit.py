ea70cea7e6c88b61dbe772959e0a6c7dcda51f54
Perry Lorier <github@isomer.meta.net.nz>
2008-04-21 22:07:59 +0000

    Submit a new discid to musicbrainz
    git-svn-id: http://source.meta.net.nz/svn/mp3parser/trunk@24 4640f49e-d398-488d-9bdf-65a890809393

---
+++ submit.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import toc
+import sys
+import os
+import urlparse
+import urllib
+import discid
+
+def parse_toc(srcpath):
+        if os.path.exists(os.path.join(srcpath, "data.toc")):
+                tocfilename = "data.toc"
+        elif os.path.exists(os.path.join(srcpath, "TOC")):
+                tocfilename = "TOC"
+        else:   
+                print "No TOC in source path!"
+                sys.exit(4)
+
+        return toc.Disc(cdrdaotocfile=os.path.join(srcpath, tocfilename))
+
+disctoc=parse_toc(sys.argv[1])
+
+print "Title:",disctoc.title
+print "Performer:",disctoc.performer
+print urlparse.urlunparse((
+	'http',
+	'musicbrainz.org',
+	'/bare/cdlookup.html',
+	'',
+	urllib.urlencode({ 'id' : discid.generate_musicbrainz_discid(
+			disctoc.get_first_track_num(),
+			disctoc.get_last_track_num(),
+			disctoc.get_track_offsets()),
+	  'toc' : " ".join(map(str,[
+		disctoc.get_first_track_num(),
+		disctoc.get_last_track_num()]+disctoc.get_track_offsets())),
+	  'tracks' : len(disctoc.get_track_offsets()) }),
+	''
+))
