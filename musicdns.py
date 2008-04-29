52b618fe6ca71cb6151d71b4b47d24f2a238a40f
Scott Raynel <scottraynel@gmail.com>
2008-04-29 12:24:07 +0000

    Forgot to add musicdns.py, an interface to the MusicDNS web-service.
    git-svn-id: http://source.meta.net.nz/svn/mp3parser/trunk@44 4640f49e-d398-488d-9bdf-65a890809393

---
+++ musicdns.py
@@ -0,0 +1,55 @@
+#
+# musicdns.py
+# An interface to the MusicDNS web-service
+#
+# Mostly stolen^Hborrowed from pyofa
+#
+
+import urllib
+from xml.etree import ElementTree
+import re
+
+musicdns_host = 'ofa.musicdns.org'
+musicdns_port = 80
+
+def lookup_fingerprint(fingerprint, duration, musicdns_key):
+	""" Given a fingerprint and duration, lookup the track using the 
+	MusicDNS web-service and return the PUID, if found.
+	"""
+	req = '/ofa/1/track'
+	url = 'http://%s:%d%s' % (musicdns_host, musicdns_port, req)
+	postargs = dict(
+		cid=musicdns_key,
+		cvr="flacnamer 0.1",
+		fpt=fingerprint,
+		art='',
+		ttl='track',
+		alb='',
+		tnm='',
+		gnr='',
+		yrr='',
+		brt='',
+		fmt='',
+		dur=str(duration),
+		rmd='1' # Return metadata, 
+	)
+	
+	data = urllib.urlencode(postargs)
+
+	f = urllib.urlopen(url, data)
+	tree = ElementTree.parse(f)
+	sanitize_tree(tree)
+	el = tree.find('//title')
+	title = el.text if el is not None else None
+
+	el = tree.find('//artist/name')
+	artist = el.text if el is not None else None
+
+	el = tree.find('//puid')
+	puid = el.attrib['id'] if el is not None else None
+
+	return (title, artist, puid)
+
+def sanitize_tree(tree):
+	for el in tree.getiterator():
+		el.tag = re.sub('{.*}', '', el.tag) 
