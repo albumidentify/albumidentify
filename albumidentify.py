6ef2c5c47f73746d0934c9b7ebfc8fadb56e531b
Perry Lorier <github@isomer.meta.net.nz>
2008-07-06 03:18:24 +0000

    Various debugging code moved around
    git-svn-id: http://source.meta.net.nz/svn/mp3parser/trunk@62 4640f49e-d398-488d-9bdf-65a890809393

--- albumidentify.py
+++ albumidentify.py
@@ -8,7 +8,6 @@ import flacnamer
 key = 'a7f6063296c0f1c9b75c7f511861b89b'
 
 def decode(frommp3name, towavname):
-	print "Decoding %(frommp3name)s" % locals()
 	os.system("mpg123 --quiet --wav \"%(towavname)s\" \"%(frommp3name)s\"" % locals())
 
 
@@ -18,22 +17,26 @@ def get_dir_info(dirname):
 	tracknum=0
 	trackinfo={}
 	for i in files:
-		tracknum=tracknum+1
 		if not i.endswith(".mp3"):
 			print "Skipping non mp3 file",`i`
 			continue
+		tracknum=tracknum+1
 		fname=os.path.join(dirname,i)
 		# While testing this uses a fixed name in /tmp
 		# and checks if it exists, and doesn't decode if it does.
 		# This is for speed while debugging, should be changed with
 		# tmpname later
-		toname=os.path.join("/tmp/",i[:-3]+"wav")
+		#toname=os.path.join("/tmp/",i[:-3]+"wav")
+		toname=os.path.join("/tmp/tmp.wav")
 		if not os.path.exists(toname):
 			decode(fname,toname)
 		(fp, dur) = fingerprint.fingerprint(toname)
+		os.unlink(toname)
 
 		(trackname, artist, puid) = musicdns.lookup_fingerprint(fp, dur, key)
+		print "***",tracknum,`artist`,`trackname`,puid
 		tracks = flacnamer.get_tracks_by_puid(puid)
+		#print [ y.title for x in tracks for y in x.releases]
 		trackinfo[tracknum]=(fname,artist,trackname,dur,tracks)
 	return trackinfo
 
@@ -45,21 +48,29 @@ def guess_album(trackinfo):
 	possible_releases={}
 	for (tracknum,(fname,artist,trackname,dur,tracks)) in trackinfo.items():
 		for track in tracks:
-			for r in track.getReleases():
+			#print "***",tracknum,`artist`,`trackname`
+			for r in track.releases:
+				#print `r.title`
 				if tracknum>1 and (
 					r.id not in possible_releases 
 					or possible_releases[r.id] != tracknum-1):
 					# Skip this album -- we know it's not going to
 					# be a final candidate
+					#print "skipping already worthless",r.id
 					continue
 				# Get the information about this release
 				release = flacnamer.get_release_by_releaseid(r.id)
 				# Skip if this album has the wrong number of tracks.
-				if len(release.getTracks()) != len(trackinfo):
+				if len(release.tracks) != len(trackinfo):
+					#print release.title,"has wrong number of tracks (expected",len(trackinfo)," not ",len(release.tracks),")"
+					#for i in release.tracks:
+					#	print ">",i.title
 					continue
 				# Skip if the tracks in the wrong place on this album
-				if flacnamer.track_number(release.getTracks(), track.title) != tracknum:
+				if flacnamer.track_number(release.tracks, track.title) != tracknum:
+					#print release.title,"doesn't have track in right position"
 					continue
+				#print release.title,"looks ok!"
 				if release.id in possible_releases:
 					possible_releases[release.id] += 1
 				else:
@@ -68,7 +79,7 @@ def guess_album(trackinfo):
 
 	for rid in [x for x in possible_releases if possible_releases[x]==len(trackinfo)]:
 		release = flacnamer.get_release_by_releaseid(rid)
-		albumartist=release.getArtist()
+		albumartist=release.artist
 		#print albumartist.name,":",release.title+" ("+rid+")"
 		releaseevents=release.getReleaseEvents()
 		#print "Release dates:"
@@ -98,6 +109,6 @@ def guess_album(trackinfo):
 
 if __name__=="__main__":
 	trackinfo=get_dir_info(sys.argv[1])
+	#print trackinfo
 	print guess_album(trackinfo)
-	#return releasedata
 
