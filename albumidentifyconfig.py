a3ca3698a9c3fe138b3757d858f9926d86977ef3
root <perry@coders.net>
2008-12-14 13:13:53 +1300

    Add config file for puidsubmission

---
+++ albumidentifyconfig.py
@@ -0,0 +1,16 @@
+import ConfigParser
+import os
+
+config=ConfigParser.ConfigParser()
+
+def readconfig():
+	config.add_section("albumidentify")
+	config.set("albumidentify","push_shortcut_puids","False")
+	config.read(os.path.expanduser("~/.albumidentifyrc"))
+	
+if __name__=="__main__":
+	readconfig()
+	for section in config.sections():
+		print "Section",section
+		for option in config.options(section):
+			print "",option,config.get(section,option)
