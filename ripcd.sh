f6d935fc7e060cf6a7242528dcb96d89a2953c30
shweppsie <shweppsie@gmail.com>
2009-04-23 15:59:18 +1200

    don't make the folder if the parameters are incorrect

--- ripcd.sh
+++ ripcd.sh
@@ -2,27 +2,35 @@
 
 dir=$(date +%Y%m%d%H%M%S)
 
-function stuff
+function start
+{
+	mkdir cd-$dir
+	pushd cd-$dir
+}
+
+function finish
 {
 	cueconvert data.toc data.cue
 	bchunk -s -w data.bin data.cue track
+	popd cd-$dir
 }
 
-mkdir cd-$dir
-pushd cd-$dir
 
 if [ "$#" -eq "1" ]; then
+	begin
 	cdrdao read-cd --device $1 --with-cddb data.toc
 	cdrecord dev=$1 -toc > cdrecord.toc
 	eject $1
-	stuff
+	finish
 elif [ "$#" -eq "0" ]; then
+	begin
+	mkdir cd-$dir
+	pushd cd-$dir
 	cdrdao read-cd --with-cddb data.toc
 	cdrecord -toc > cdrecord.toc
 	eject
-	stuff
+	finish
 else
 	echo -e "Usage: ripcd.sh [device]"
 fi
 
-popd cd-$dir
