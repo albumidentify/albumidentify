4aec585653151289a10b1681018333d061b6f874
shweppsie <shweppsie@gmail.com>
2009-04-23 15:43:40 +1200

    Added the ability to specify device when ripping cd's

--- ripcd.sh
+++ ripcd.sh
@@ -2,11 +2,27 @@
 
 dir=$(date +%Y%m%d%H%M%S)
 
+function stuff
+{
+	cueconvert data.toc data.cue
+	bchunk -s -w data.bin data.cue track
+}
+
 mkdir cd-$dir
 pushd cd-$dir
-cdrdao read-cd --with-cddb data.toc
-cdrecord -toc > cdrecord.toc
-eject
-cueconvert data.toc data.cue
-bchunk -s -w data.bin data.cue track
+
+if [ "$#" -eq "1" ]; then
+	cdrdao read-cd --device $1 --with-cddb data.toc
+	cdrecord dev=$1 -toc > cdrecord.toc
+	eject $1
+	stuff
+elif [ "$#" -eq "0" ]; then
+	cdrdao read-cd --with-cddb data.toc
+	cdrecord -toc > cdrecord.toc
+	eject
+	stuff
+else
+	echo -e "Usage: ripcd.sh [device]"
+fi
+
 popd cd-$dir
