4855a47f8635922de08b8c689dffb574841fa8a3
Perry Lorier <perry@coders.net>
2009-05-03 11:36:33 +1200

    Count webservice failures

--- flacreport.py
+++ flacreport.py
@@ -27,6 +27,7 @@ fails = 0
 unprocessed = 0
 unknown = 0
 too_short = 0
+webfail=0
 notdir=0
 
 for d in sys.argv[1:]:
@@ -69,7 +70,19 @@ for d in sys.argv[1:]:
 	elif s.find("This disc is part") != -1:
 		multi = multi + 1
 		failure = 1
-	elif s.find("exception") != -1 or s.find("fail!") != -1:
+	elif s.find("WebServiceError: HTTP Error 503: Service Temporarily Unavailable") != -1 \
+	  or s.find("WebServiceError: HTTP Error 502: Bad Gateway") != -1 \
+	  or s.find("ResourceNotFoundError: HTTP Error 404: Not Found") != -1 \
+	  or s.find("IOError: ('http protocol error', 0, 'got a bad status line', None)") != -1 \
+	  or s.find("IOError: [Errno socket error] (104, 'Connection reset by peer'") != -1:
+		webfail += 1
+		failure = 1
+		log("Webservice failure:",d)
+	elif s.find("UnicodeDecodeError") != -1:
+		unknown += 1
+		failure = 1
+		log("Unicode failure")
+	elif s.find("exception") != -1 or s.find("fail!") != -1 or s.find("Traceback") != -1:
 		unknown = unknown + 1
 		failure = 1
 		log("Unknown:",d)
@@ -87,6 +100,7 @@ print "  No Release   : %3d" % (no_match)
 print "  Unknown year : %3d" % (unk_year)
 print "  Ambig. Discid: %3d" % (amb_discid)
 print "  Multi disc   : %3d" % (multi)
+print "  Lookup Fail  : %3d" % (webfail)
 print "  Unknown Fails: %3d" % (unknown)
 print "  Too short    : %3d" % (too_short)
 print "Successful     : %3d (%.02f%%)" % (success,success*100.0/total)
