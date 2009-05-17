7754cd34caddf6399163fee3bd47789d692cbec6
Nathan <shweppsie@gmail.com>
2009-05-17 21:12:14 +1200

    updated ripcd.sh

--- ripcd.sh
+++ ripcd.sh
@@ -2,34 +2,15 @@
 
 dir=$(date +%Y%m%d%H%M%S)
 
-function begin
-{
-	mkdir cd-$dir
-	pushd cd-$dir
-}
-
-function finish
-{
-	cueconvert data.toc data.cue
-	bchunk -s -w data.bin data.cue track
-	popd cd-$dir
-}
-
-
 if [ "$#" -eq "1" ]; then
-	begin
+	mkdir cd-$dir
+	pushd cd-$dir	
 	cdrdao read-cd --device $1 --with-cddb data.toc
 	cdrecord dev=$1 -toc > cdrecord.toc
 	eject $1
-	finish
-elif [ "$#" -eq "0" ]; then
-	begin
-	mkdir cd-$dir
-	pushd cd-$dir
-	cdrdao read-cd --with-cddb data.toc
-	cdrecord -toc > cdrecord.toc
-	eject
-	finish
+	cueconvert data.toc data.cue
+	bchunk -s -w data.bin data.cue track
+	popd cd-$dir
 else
 	echo -e "Usage: ripcd.sh [device]"
 fi
