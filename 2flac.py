cb46ccae7c6c596c777d74c7c56c233ff02a5c1b
Scott Raynel <scottraynel@gmail.com>
2009-09-21 00:32:19 +1200

    Add 2flac.py, a smarter encoder script.
    This script replaces the function of 2flac.sh and takes advantage of
    multiple CPUs.
    
    Example:
    
    ./2flac.py --dest-path=flacs/ -j 2 raw/cd-*
    
    Will encode raw/cd-* into flacs/ using 2 encoder jobs.

---
+++ 2flac.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+import sys
+import Queue
+import optparse
+import subprocess
+import os
+import shutil
+import threading
+
+q = Queue.Queue()
+
+def process_item(path):
+        dir = os.path.basename(path)
+        src = path
+        dst = os.path.join(options.destpath, dir)
+
+        # If dest exists, skip
+        if os.path.exists(dst):
+                print "%s already exists" % dst
+                return
+
+        # XXX We should do some better sanity checks here. For example, if
+        # the dest path exists, we should check that there is
+        #  a) the same number of flacs as wavs
+        #  b) that the flacs verify
+        #  c) that the flac dir has a tocfile
+
+        proclist = ["flac",
+                    "--verify",
+                    "--replay-gain",
+                    "--max-lpc-order=12",
+                    "--blocksize=4096",
+                    "--mid-side",
+                    "--exhaustive-model-search",
+                    "--rice-partition-order=6",
+                    "--qlp-coeff-precision-search",
+                    "--padding=131027",
+                    "--totally-silent"
+                    ]
+
+        for f in [x for x in os.listdir(src) if x.endswith(".wav")]:
+                proclist.append(os.path.join(src, f))
+
+        p = subprocess.Popen(proclist)
+        p.communicate()
+
+        print "%s completed with returncode %i" % (src, p.returncode)
+
+        if (p.returncode != 0):
+                # Clean up any mess that flac left
+                for f in [x for x in os.listdir(src) if x.endswith(".flac")]:
+                        os.unlink(os.path.join(src,f))
+                return
+
+        # Move the resulting flacs to destdir
+        os.mkdir(dst)
+        os.system("mv %s %s" % (os.path.join(src, "*.flac"), dst))
+
+        # Copy the tocfile if it exists
+        tocfilename = ""
+        if os.path.exists(os.path.join(src, "TOC")):
+                tocfilename = os.path.join(src, "TOC")
+        if os.path.exists(os.path.join(src, "data.toc")):
+                tocfilename = os.path.join(src, "data.toc")
+
+        if tocfilename:
+                shutil.copy(tocfilename, os.path.join(dst, "data.toc"))
+        
+
+def worker():
+        t = threading.currentThread()
+        while not q.empty():
+                item = q.get()
+                print "%s encoding %s" % (t.name, item)
+                process_item(item)
+                q.task_done()
+
+def path_arg_cb(option, opt_str, value, parser):
+        path = os.path.abspath(value)
+        if not os.path.isdir(path):
+                raise optparse.OptionValueError("to %s must be a directory that exists" % path)
+        setattr(parser.values, option.dest, path)
+
+opts = optparse.OptionParser(usage="%s [options] <srcdirs>" % sys.argv[0])
+opts.add_option(
+        "-d", "--dest-path",
+        type="str",
+        dest="destpath",
+        default="flacs/",
+        metavar="PATH",
+        action="callback",
+        callback=path_arg_cb,
+        help="Prefix output directories with PATH"
+)
+
+opts.add_option(
+        "-j",
+        type="int",
+        dest="numcpus",
+        default=1,
+        metavar="THREADS",
+        help="Spawn multipled THREADS for encoding"
+)
+
+(options,args) = opts.parse_args()
+
+# Check that src paths exist
+for path in args:
+        if not os.path.exists(os.path.abspath(path)):
+                print("%s doesn't exist!" % path)
+                opts.print_help()
+                sys.exit(1)
+
+# Queue paths for work
+for path in args:
+        if os.path.isdir(path):
+                q.put(path)
+
+# Spawn worker threads to deal with the work
+for i in range(options.numcpus):
+        t = threading.Thread(target=worker)
+        t.setDaemon(True)
+        t.start()
+
+# Wait until all threads have finished (queue is processed)
+q.join()
+print "All worker threads finished"
+
+
