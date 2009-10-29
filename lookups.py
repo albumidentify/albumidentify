3883e8b31f7798b736ba0d01c1d43d89580cd3bb
Scott Raynel <scottraynel@gmail.com>
2009-10-29 22:43:35 +1300

    Modify the 503 backoff so that it also catches urlopen timeouts.

--- lookups.py
+++ lookups.py
@@ -38,8 +38,12 @@ webservices = {
 	},
 }
 
-def musicbrainz_503_cooldown():
-        " Decorator to catch 503s and cooldown before calling the function again. "
+def musicbrainz_retry():
+        """ Decorator to retry failures in musicbrainz calls.
+
+            For example, a 503 will result in a 20 second cooldown. A urlopen timeout
+            will simply be tried again.
+        """
         def ws_backoff(func):
                 def backoff_func(*args, **kwargs):
                         global lastwsquery
@@ -48,16 +52,24 @@ def musicbrainz_503_cooldown():
                         except ws.WebServiceError,e:
                                 if (e.msg.find("503") != -1):
                                         util.update_progress("Caught musicbrainz 503, waiting 20s and trying again...")
-                                        time.sleep(20)
-                                        # Reset the timer delayed uses so that we don't
-                                        # end up with a bunch of queries causing
-                                        # another 503
-                                        lastwsquery["musicbrainz"]=time.time()
-                                        return func(*args,**kwargs)
+                                else:
+                                        raise e
+                        except ws.ConnectionError,e:
+                                if (e.msg.find("urlopen error timed out") != -1):
+                                        util.update_progress("Caught musicbrainz urlopen timeout. Retrying...")
                                 else:
                                         raise e
                         except Exception,e:
                                 raise e
+
+                        time.sleep(20)
+                        # Reset the timer delayed uses so that we don't
+                        # end up with a bunch of queries causing
+                        # another 503
+                        lastwsquery["musicbrainz"]=time.time()
+                        # Retry the call
+                        return func(*args,**kwargs)
+
                 backoff_func.__name__=func.__name__
                 return backoff_func
         return ws_backoff
@@ -105,7 +117,7 @@ if SUBMIT_SUPPORT:
 	trackincludes["isrcs"]=True
 	
 @memocache.memoify()
-@musicbrainz_503_cooldown()
+@musicbrainz_retry()
 @delayed("musicbrainz")
 def get_tracks_by_puid(puid):
 	""" Lookup a list of musicbrainz tracks by PUID. Returns a list of Track
@@ -119,7 +131,7 @@ def get_tracks_by_puid(puid):
 	return results
 
 @memocache.memoify()
-@musicbrainz_503_cooldown()
+@musicbrainz_retry()
 @delayed("musicbrainz")
 def get_track_by_id(id):
 	q = ws.Query()
@@ -130,7 +142,7 @@ def get_track_by_id(id):
 	return t
 
 @memocache.memoify()
-@musicbrainz_503_cooldown()
+@musicbrainz_retry()
 @delayed("musicbrainz")
 def get_release_by_releaseid(releaseid):
 	""" Given a musicbrainz release-id, fetch the release from musicbrainz. """
@@ -150,7 +162,7 @@ def get_release_by_releaseid(releaseid):
 	return q.getReleaseById(id_ = releaseid, include=includes)
 
 @memocache.memoify()
-@musicbrainz_503_cooldown()
+@musicbrainz_retry()
 @delayed("musicbrainz")
 def get_releases_by_cdtext(title, performer, num_tracks):
 	""" Given the performer, title and number of tracks on a disc, lookup
@@ -170,7 +182,7 @@ the empty list if there were no matches. """
 		]
 
 @memocache.memoify()
-@musicbrainz_503_cooldown()
+@musicbrainz_retry()
 @delayed("musicbrainz")
 def get_releases_by_discid(discid):
         """ Given a musicbrainz disc-id, fetch a list of possible releases. """
@@ -189,7 +201,7 @@ def track_number(tracks, track):
 	return -1
 
 @memocache.memoify()
-@musicbrainz_503_cooldown()
+@musicbrainz_retry()
 @delayed("musicbrainz")
 def get_track_artist_for_track(track):
 	""" Returns the musicbrainz Artist object for the given track. This may
