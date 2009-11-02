4cd21c466ddee788a8e8f367ef979252ac3ba5c1
Perry Lorier <perry@coders.net>
2009-11-02 22:49:49 +1300

    Don't die on lastfm lookup failures

--- md_lastfm.py
+++ md_lastfm.py
@@ -1,9 +1,16 @@
 import lastfm
 import tag
+import urllib2
 
 def get_tags(tags, mbalbum, mbtrack, artistname):
-	artist_tags = lastfm.get_artist_toptags(artistname)
-	track_tags =  lastfm.get_track_toptags(artistname,mbtrack.title,mbtrack.id)
+	try:
+		artist_tags = lastfm.get_artist_toptags(artistname)
+	except urllib2.HTTPError, e:
+		artist_tags = {}
+	try:
+		track_tags =  lastfm.get_track_toptags(artistname,mbtrack.title,mbtrack.id)
+	except urllib2.HTTPError, e:
+		track_tags = {}
 	taglist = [
 		i["name"][0]
 		for i in 
