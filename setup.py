7f7e7e1723c2f0ef24ed3fb891b1a994f1c4b5c8
Scott Raynel <scottraynel@gmail.com>
2010-09-22 21:50:38 +1200

    Update setup.py to install necessary files.
    This really needs to be tidied up, e.g. we should really have a
    'albumidentify' dir under dist-packages so that things with
    generic names such as util.py don't potentially conflict with
    other python packages.

--- setup.py
+++ setup.py
@@ -9,7 +9,11 @@ setup(name="albumidentify",
         py_modules = ['albumidentify', 'albumidentifyconfig', 'amazon4', \
                       'discid', 'fingerprint', 'libofa', 'lookups', \
                       'mp3names', 'musicbrainz', 'musicdns', 'parsemp3', \
-                      'puidsubmit', 'submit', 'tag', 'toc', 'sort'],
+                      'puidsubmit', 'submit', 'tag', 'toc', 'sort', \
+                      'musicfile', 'serialisemp3', 'memocache', 'util', \
+                      'strat_transitive', 'strat_metadata', 'strat_musicbrainzid', \
+                      'strat_trackname', 'replaygain', 'ripcd', 'toflac', \
+                      'md_genre', 'md_lastfm', 'md_musicbrainz', 'lastfm'  ],
         scripts = ['renamealbum']
         )
         
