fa3d2a13d934a6b8c5e65f6be5d607efa753296a
Alastair Porter <alastair@porter.net.nz>
2010-09-25 20:44:09 -0400

    Update setup to install the package + scripts

--- setup.py
+++ setup.py
@@ -6,14 +6,8 @@ setup(name="albumidentify",
         version="0.0.1",
         description="Tools to identify and manage music albums",
         url="http://www.github.com/scottr/albumidentify",
-        py_modules = ['albumidentify', 'albumidentifyconfig', 'amazon4', \
-                      'discid', 'fingerprint', 'libofa', 'lookups', \
-                      'mp3names', 'musicbrainz', 'musicdns', 'parsemp3', \
-                      'puidsubmit', 'submit', 'tag', 'toc', 'sort', \
-                      'musicfile', 'serialisemp3', 'memocache', 'util', \
-                      'strat_transitive', 'strat_metadata', 'strat_musicbrainzid', \
-                      'strat_trackname', 'replaygain', 'ripcd', 'toflac', \
-                      'md_genre', 'md_lastfm', 'md_musicbrainz', 'lastfm'  ],
-        scripts = ['renamealbum']
+        package_dir = {'renamealbum': 'src/renamealbum'},
+        scripts = ['renamealbum', 'ripcd', 'toflac'],
+	packages = ['renamealbum']
         )
         
