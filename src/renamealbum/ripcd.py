a611b07e0396236e4845384a5467c8859b3a2d54
Nathan Overall <shweppsie@gmail.com>
2011-05-18 03:20:18 +1200

    Add continuous feature to ripcd. closes #73
    * Added CD.py library to detect CDROM tray status
    * CD.py currently only supports linux
    * Updated ripcd to use optparse for parsing command line args
    * Ignores ctrl+c when in -c mode and not doing anything

--- src/renamealbum/ripcd.py
+++ src/renamealbum/ripcd.py
@@ -4,13 +4,13 @@ import os
 import subprocess
 import sys
 import time
+import CD
 
 class CDRipFailedException(Exception):
         def __init__(self,message):
                 self.message=message
         def __str__(self):
                 return self.message
-
 
 def rip_cd(device, destpath):
         oldwd = os.getcwd()
@@ -46,18 +46,59 @@ def rip_cd(device, destpath):
                 raise CDRipFailedException("bchunk failed with returncode %i" % p3.returncode)
 
         os.chdir(oldwd)
-        return True
 
 def main():
-    if len(sys.argv) == 3:
-        rip_cd(sys.argv[1], sys.argv[2])
-    elif len(sys.argv) == 2:
-	dest = "cd-%s" % (time.strftime("%Y%m%d%H%M%S"),)
-        os.mkdir(dest)
-        rip_cd(sys.argv[1], dest)
+    import optparse
+    opts = optparse.OptionParser(usage="%s <device> [destination]" % sys.argv[0])
+    opts.add_option("-c",
+	    "--continuous",
+	    dest="continuous",
+	    default=False,
+	    action="store_true",
+	    help="After each rip eject the tray and wait for more cds. Ripping will start as soon as another cd is inserted"
+    )
+
+    (options, args) = opts.parse_args()
+
+    if len(args) == 2:
+	destdir = args[1]
+    elif len(args) == 1:
+	destdir = os.getcwd()
     else:
-        print("Usage: %s <device> [destination]" % (sys.argv[0],))
-        sys.exit(1)
+	opts.print_help()
+	sys.exit(2)
+
+    device = args[0]
+
+    if not options.continuous:
+	dest = os.path.join(destdir,("cd-%s" % (time.strftime("%Y%m%d%H%M%S"),)))
+	if not os.path.exists(dest):
+		os.mkdir(dest)
+	rip_cd(device,dest)
+    else:
+	try:
+		CD.status(device)
+	except CD.PlatformNotSupportedException, e:
+		print "-c is not currently supported on your platform"
+		sys.exit(1)
+	print "Press Ctrl+c to exit"
+	while True:
+		if CD.status(device) == 1:
+			dest = os.path.join(destdir,("cd-%s" % (time.strftime("%Y%m%d%H%M%S"),)))
+			if not os.path.exists(dest):
+				os.mkdir(dest)
+			# backoff an extra 2 seconds
+			try:
+				time.sleep(2)
+			except KeyboardInterrupt, e:
+				sys.exit(0)
+			rip_cd(device,dest)
+			CD.eject(device)
+		else:
+			try:
+				time.sleep(2)
+			except KeyboardInterrupt, e:
+				sys.exit(0)
 
 if __name__ == "__main__":
-	main()
+    main()
