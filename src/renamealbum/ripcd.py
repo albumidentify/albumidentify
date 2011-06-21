9debb103e0c9ee59086bc8b97fd6f9428039cdda
Nathan Overall <shweppsie@gmail.com>
2011-06-21 19:34:17 +1200

    Fix tabbing in ripcd

--- src/renamealbum/ripcd.py
+++ src/renamealbum/ripcd.py
@@ -7,97 +7,99 @@ import time
 import blockdevice
 
 class CDRipFailedException(Exception):
-        def __init__(self,message):
-                self.message=message
-        def __str__(self):
-                return self.message
+	def __init__(self,message):
+		self.message=message
+	def __str__(self):
+		return self.message
 
 def rip_cd(device, destpath):
-        oldwd = os.getcwd()
-        print "ripping device %s to %s" % (device, destpath)
-        os.chdir(destpath)
+	oldwd = os.getcwd()
+	print "ripping device %s to %s" % (device, destpath)
+	os.chdir(destpath)
 
-        proclist = ["cdrdao", "read-cd", "--with-cddb", "--device", device, "data.toc"]
+	proclist = ["cdrdao", "read-cd", "--with-cddb", "--device", device, "data.toc"]
 	try:
-	        p = subprocess.Popen(proclist)
-	        p.communicate()
+		p = subprocess.Popen(proclist)
+		p.communicate()
 	except OSError, e:
 		raise CDRipFailedException("cdrdao not installed. Cannot rip CD")
 
-        if p.returncode != 0:
-                raise CDRipFailedException("cdrdao failed with returncode %i" % p.returncode)
+	if p.returncode != 0:
+		raise CDRipFailedException("cdrdao failed with returncode %i" % p.returncode)
 
 	try:
-	        p2 = subprocess.Popen(["cueconvert", "data.toc", "data.cue"])
-	        p2.communicate()
+		p2 = subprocess.Popen(["cueconvert", "data.toc", "data.cue"])
+		p2.communicate()
 	except OSError, e:
 		raise CDRipFailedException("cueconvert not installed. Cannot rip CD")
 
-        if p2.returncode != 0:
-                raise CDRipFailedException("cueconvert failed with returncode %i" % p2.returncode)
+	if p2.returncode != 0:
+		raise CDRipFailedException("cueconvert failed with returncode %i" % p2.returncode)
 
 	try:
-	        p3 = subprocess.Popen(["bchunk", "-s", "-w", "data.bin", "data.cue", "track"])
-	        p3.communicate()
+		p3 = subprocess.Popen(["bchunk", "-s", "-w", "data.bin", "data.cue", "track"])
+		p3.communicate()
 	except OSError, e:
-                raise CDRipFailedException("bchunk not installed. Cannot rip CD")
+		raise CDRipFailedException("bchunk not installed. Cannot rip CD")
 
-        if p3.returncode != 0:
-                raise CDRipFailedException("bchunk failed with returncode %i" % p3.returncode)
+	if p3.returncode != 0:
+		raise CDRipFailedException("bchunk failed with returncode %i" % p3.returncode)
 
-        os.chdir(oldwd)
+	os.chdir(oldwd)
 
 def main():
-    import optparse
-    opts = optparse.OptionParser(usage="%s <device> [destination]" % sys.argv[0])
-    opts.add_option("-c",
-	    "--continuous",
-	    dest="continuous",
-	    default=False,
-	    action="store_true",
-	    help="After each rip eject the tray and wait for more cds. Ripping will start as soon as another cd is inserted"
-    )
+	import optparse
+	opts = optparse.OptionParser(usage="%s <device> [destination]" % sys.argv[0])
+	opts.add_option("-c",
+		"--continuous",
+		dest="continuous",
+		default=False,
+		action="store_true",
+		help="After each rip eject the tray and wait for more cds. Ripping will start as soon as another cd is inserted"
+	)
 
-    (options, args) = opts.parse_args()
+	(options, args) = opts.parse_args()
 
-    if len(args) == 2:
+	if len(args) == 2:
 	destdir = args[1]
-    elif len(args) == 1:
+	elif len(args) == 1:
 	destdir = os.getcwd()
-    else:
+	else:
 	opts.print_help()
 	sys.exit(2)
 
-    device = args[0]
+	device = args[0]
 
-    try:
-        cd = blockdevice.GetDevice(device)
-    except blockdevice.PlatformNotSupportedException, e:
-        print "-c is not currently supported on your platform"
-        sys.exit(1)
-    looping = True
-    while looping:
+	try:
+		cd = blockdevice.GetDevice(device)
+	except blockdevice.PlatformNotSupportedException, e:
+		print "-c is not currently supported on your platform"
+		sys.exit(1)
+	looping = True
+	while looping:
 	print "Waiting for media to rip"
-        print "Press Ctrl+c to exit"
-        while not cd.is_ready():
-            try:
-                time.sleep(2)
-            except KeyboardInterrupt, e:
-                sys.exit(0)
-        print "Device ready, preparing..."
-        dest = os.path.join(destdir,("cd-%s" % (time.strftime("%Y%m%d%H%M%S"),)))
-        if not os.path.exists(dest):
-            os.mkdir(dest)
-        # backoff an extra 2 seconds
-        try:
-            time.sleep(2)
-        except KeyboardInterrupt, e:
-            sys.exit(0)
-        print "Beginning Rip..."
-        rip_cd(device,dest)
-        cd.eject()
-        if not options.continuous:
-            looping = False
+		print "Press Ctrl+c to exit"
+		while not cd.is_ready():
+			try:
+				time.sleep(2)
+			except KeyboardInterrupt, e:
+				sys.exit(0)
+		print "Device ready, preparing..."
+		dest = os.path.join(destdir,("cd-%s" % (time.strftime("%Y%m%d%H%M%S"),)))
+		if not os.path.exists(dest):
+			os.mkdir(dest)
+		# backoff an extra 2 seconds
+		try:
+			time.sleep(2)
+		except KeyboardInterrupt, e:
+			sys.exit(0)
+		print "Beginning Rip..."
+		rip_cd(device,dest)
+		cd.eject()
+		if not options.continuous:
+			looping = False
 
 if __name__ == "__main__":
-    main()
+	main()
+
+# vim: set sw=8 tabstop=8 softtabstop=8 noexpandtab :
